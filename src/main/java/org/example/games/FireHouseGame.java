09ac795966ccf9b29ebd0e930e33e6c9b94395e7
Francesco <franreg2002@gmail.com>
2024-03-04 22:48:18 +0100

    Modifiche minori nei file delle classi.

--- src/main/java/org/example/games/FireHouseGame.java
+++ src/main/java/org/example/games/FireHouseGame.java
@@ -31,7 +31,7 @@ public class FireHouseGame extends GameDescription {
     @Override
     public void init() throws Exception {
         //Commands
-        Command nord = new Command(CommandType.NORD, "nord");
+        Command nord = new Command(CommandType.NORTH, "nord");
         nord.setAlias(new String[]{"n", "N", "Nord", "NORD"});
         getCommands().add(nord);
         Command iventory = new Command(CommandType.INVENTORY, "inventario");
@@ -120,7 +120,7 @@ public class FireHouseGame extends GameDescription {
             //move
             boolean noroom = false;
             boolean move = false;
-            if (p.getCommand().getType() == CommandType.NORD) {
+            if (p.getCommand().getType() == CommandType.NORTH) {
                 if (getCurrentRoom().getNorth() != null) {
                     setCurrentRoom(getCurrentRoom().getNorth());
                     move = true;
