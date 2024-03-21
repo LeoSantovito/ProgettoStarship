9a9baa4db97181313643a2b5b26f6ad473a9bfd5
Francesco <franreg2002@gmail.com>
2024-03-21 11:24:39 +0100

    Modifiche a stile frame.

--- src/main/java/org/example/swing/MainFrame.java
+++ src/main/java/org/example/swing/MainFrame.java
@@ -15,6 +15,8 @@ public class MainFrame extends javax.swing.JFrame {
      */
     public MainFrame() {
         initComponents();
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
     }
 
     /**
