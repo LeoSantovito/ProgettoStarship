e333fd886d34c020feaeb5de3398d22543e53aa1
Francesco <franreg2002@gmail.com>
2024-03-23 22:03:35 +0100

    Rimossa modifica.

--- src/main/java/org/example/Engine.java
+++ src/main/java/org/example/Engine.java
@@ -153,7 +153,7 @@ public class Engine {
             System.out.println("Hai già giocato " + Utils.printGameTime(totalSeconds) + "! È un po' tanto per questo gioco...");
         }
 
-        System.out.println("Vabbè, eri nella stanza: " + game.getCurrentRoom().getName() + ".");
+        System.out.println("Sei nella stanza: " + game.getCurrentRoom().getName() + ".");
         System.out.println();
         System.out.println(game.getCurrentRoom().getDescription());
         System.out.println();
